5ce9b58d550e13deae5ec8f3ab1556d4b3caf7d5
Philipbear <philipxsp@hotmail.com>
2024-09-05 13:20:36 -0700

    empty df

--- bin/create_library/main.py
+++ bin/create_library/main.py
@@ -26,7 +26,7 @@ def create_library(compound_df, feature_df,
     # Merge the compound and feature tables
     df = merge_compound_feature_tables(compound_df, feature_df)
 
-    if not df:
+    if df is None or df.empty:
         return None, None
 
     if filter_library:
